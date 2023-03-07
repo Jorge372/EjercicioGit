6236683b7c44ee570353deebd286231f1bfb9838
Jorge372 <127211873+Jorge372@users.noreply.github.com>
2023-03-07 15:51:45 -0300

    Segunda version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numeros[] = {1,8,6,5,5};
+        int contador = 0;
+        for (int n : numeros){
+            if (n<4)
+                contador++;
+        }
+        System.out.println("Hay "+contador+" alumno/s desaprobado/s");
     }
 }
